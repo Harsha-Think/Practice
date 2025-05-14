36ad02171c89ed91ec9fb73d1c0e514defc37328
Harsha <Harshaguntaka55@gmail.com>
2025-05-14 12:38:14 +0530

    Add files via upload

---
+++ Assignment - 2.py
@@ -0,0 +1,15 @@
+# Check if a Number is Even or Odd # Task - 1
+'''
+n=int(input("Enter a number - "))
+if n % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+'''
+
+# Sum of Integers from 1 to 50 Using a Loop # Task - 1
+
+Total=0
+for number in range(1, 51):
+    Total += number
+print("The sum of numbers from 1 to 50 is:", Total)
